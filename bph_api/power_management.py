e0fea935457d4e6d52da4e07fbb2d202e0b0aca9
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2018-10-29 09:35:22 +0100

    bph_api: power measurement function

--- bph_api/power_management.py
+++ bph_api/power_management.py
@@ -1,8 +1,11 @@
 import wiringpi as wpi
+from time import sleep
+from driver_ina226 import INA226
 
 class PowerManagement:
 
-    def __init__(self, usb_pin, ext_pin, conf = 'USB'):
+    def __init__(self, usb_pin, ext_pin, bp_rst_pin, measurement_shunt,
+                 conf = 'USB'):
         # set power enable lines as outputs
         wpi.pinMode(usb_pin, wpi.GPIO.OUTPUT)
         wpi.pinMode(ext_pin, wpi.GPIO.OUTPUT)
@@ -10,6 +13,10 @@ class PowerManagement:
         self.usb_pin = usb_pin
         self.ext_pin = ext_pin
         self.set_power_conf(conf)
+        self.bp_rst_pin = bp_rst_pin
+
+        self.meas_dev = INA226(0x44, INA226.MODE_SHUNT_BUS_CONTINUOUS,
+                               shunt_val = shunt)
 
     def set_power_conf(self, conf):
         assert conf in ('USB', 'EXT'), "Invalid power configuration '{}'"\
@@ -33,4 +40,32 @@ class PowerManagement:
         else:
             wpi.digitalWrite(self.ext_pin, _state)
 
-    
+    def get_power_measurement(self):
+        # measurements must be done with external power
+        prev_conf = self.power_conf
+        self.set_power_conf('EXT')
+
+        # TODO check this mode get
+        prev_mode = wpi.getAlt(self.bp_rst_pin)
+        prev_state = wpi.digitalRead(self.bp_rst_pin)
+
+        # reset blue pill so power is not drained from gpios
+        wpi.pinMode(self.bp_rst_pin, wpi.GPIO.OUTPUT)
+        wpi.digitalWrite(self.bp_rst_pin, wpi.GPIO.LOW)
+
+        # wait for a full conversion time
+        # this may change if average on INA226 is increased
+        sleep(2.2 / 1000)
+
+        voltage = self.meas_dev.get_shunt_voltage()
+        current = self.meas_dev.get_current()
+        power = self.meas_dev.get_power()
+
+        # restore the blue pill to previous state
+        wpi.pinMode(self.bp_rst_pin, prev_mode)
+        # TODO check if this works even as output
+        wpi.digitalWrite(self.bp_rst_pin, prev_state)
+
+        # restore power configuration to previous state
+        self.set_power_conf(prev_conf)
+
